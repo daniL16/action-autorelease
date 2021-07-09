73abf851970ce865c80ed8412ed642a1f76da941
Dani Lopez Garcia <dani16lp@gmail.com>
2021-07-09 12:57:08 +0200

    Update main.sh

--- src/main.sh
+++ src/main.sh
@@ -11,6 +11,8 @@ dateLastRelease=$(github::get_lastReleaseDate)
 bodyRelease=$(github::getReleaseDescription ${dateLastRelease})
 
 git checkout -b ${branch_name};
+git -c user.name="GitHub Actions" -c user.email="actions@github.com";
+git push --set-upstream origin "HEAD:${branch_name}";
 github::create_pr ${branch_name} "Release ${version}" "${bodyRelease}"
 
 }
