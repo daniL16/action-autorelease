710a856911d8e2a003c33740b8e66e86fff7d1f2
Dani Lopez Garcia <dani16lp@gmail.com>
2021-07-10 13:15:10 +0200

    Update main.sh

--- src/main.sh
+++ src/main.sh
@@ -10,7 +10,8 @@ branch_name=release/${version}
 dateLastRelease=$(github::get_lastReleaseDate)
 bodyRelease=$(github::getReleaseDescription ${dateLastRelease})
 
-git checkout develop origin/develop;
+git fetch --all;
+git checkout develop;
 git checkout -b ${branch_name};
 git push --set-upstream origin "HEAD:${branch_name}";
 github::create_pr ${branch_name} "Release ${version}" "${bodyRelease}"
