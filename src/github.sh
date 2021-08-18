305596bd11e1d0fe36c97630aad5aecfc1f87087
Dani Lopez Garcia <dani16lp@gmail.com>
2021-08-18 08:31:57 +0200

    Update github.sh

--- src/github.sh
+++ src/github.sh
@@ -46,7 +46,7 @@ github::getReleaseDescription(){
         pull="$(echo "$pr" | base64 -d)"
         title=$(echo "$pull" | jq --raw-output '.title')
         number=$(echo "$pull" | jq --raw-output '.number')
-        link=$(echo "$pull" | jq --raw-output '.body' | grep -Eo 'https://app.clickup.com[^ >]+' | tr '\r' ' ')
+        link=$(echo "$pull" | jq --raw-output '.body' | grep -Eo 'https://app.clickup.com[^ >]+' | tr '\r' ' ' | tr -d '\n')
         releaseBody="${releaseBody} <br> #${number} ${title} ${link}"
     done
     
